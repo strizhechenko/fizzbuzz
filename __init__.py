ccd991741948d6405f47c98d2399e05383a7dcc5
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-05-24 11:35:22 +0500

    xrange instead range

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 output = range(1, 101)
 for div, word in ((3, "fizz"), (5, "buzz"), (15, "fizzbuzz")):
-    for index in range(div, 101, div):
+    for index in xrange(div, 101, div):
         output[index - 1] = word
 for n in output:
     print n
