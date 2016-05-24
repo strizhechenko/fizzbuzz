5863e64615c4ea33f5221ec99aca1fc37cd39cc1
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-05-24 11:32:11 +0500

    Fizz commit

---
+++ __init__.py
@@ -0,0 +1,6 @@
+output = range(1, 101)
+for div, word in ((3, "fizz"), (5, "buzz"), (15, "fizzbuzz")):
+    for index in range(div, 101, div):
+        output[index - 1] = word
+for n in output:
+    print n
